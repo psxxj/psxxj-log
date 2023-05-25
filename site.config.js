6152068b0b9dc3631304e843cf71247bf6475077
박성준 <shackleton@gm.gist.ac.kr>
2023-05-25 17:28:04 +0900

    init(main): init-settings

--- site.config.js
+++ site.config.js
@@ -1,32 +1,32 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "psxxj",
+    image: "/psxxj-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "full-stack developer",
+    bio: "WE NEVER WALK ALONE",
+    email: "shackleton@gm.gist.ac.kr",
+    linkedin: "shackleton",
+    github: "psxxj",
+    instagram: "psxxj",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `kidztales`,
+      href: "https://github.com/kidztales",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "psxxj-world",
+    description: "welcome to psxxj-world!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://psxxj-log.vercel.app/",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["Blog", "Website", "Notion"],
