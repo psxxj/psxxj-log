8fcdd64427cb0749cfd9c65df0240379b8d39dd3
박성준 <shackleton@gm.gist.ac.kr>
2023-05-25 18:20:48 +0900

    test

--- site.config.js
+++ site.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "psxxj/psxxj-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
