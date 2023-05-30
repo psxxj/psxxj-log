24bdf8acb17186404b1e4c328998d3b497f11656
박성준 <shackleton@gm.gist.ac.kr>
2023-05-30 16:07:57 +0900

    edit site config

--- site.config.js
+++ site.config.js
@@ -49,7 +49,7 @@ const CONFIG = {
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
